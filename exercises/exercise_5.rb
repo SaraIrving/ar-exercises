3f49c028776c3ab8243424c323c978ed89ad575c
Sara Irving <sarakirving@gmail.com>
2020-08-17 23:24:14 +0000

    output the total revenue for the company, the annual average revenue per store, and the number of stores that are generating at least 1 million annually

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# output the total revenue for the entire company using .sum method
+sum =  Store.sum(:annual_revenue)
+puts "The total annual revenue is #{sum}"
+
+# output average annual revenue for all stores
+num_stores = Store.count
+average_per_store = sum / num_stores
+puts "The average annual revenue for all stores is #{average_per_store}"
+
+# output the number of stores that are generating 1 million plys in annual sales
+mill_stores = Store.where("annual_revenue >= 1000000").count
+puts "The number of stores generating $1 million or more annually: #{mill_stores}"
