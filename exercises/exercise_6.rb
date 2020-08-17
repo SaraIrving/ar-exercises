eb7dc11dc4d865da59645c042eb90e8ca80dcd84
Sara Irving <sarakirving@gmail.com>
2020-08-17 23:53:59 +0000

    create several employees for @store1 and @store2, specify the employees first and last names and hourly rate

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurran", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Glen", last_name: "Chua", hourly_rate: 60)
+@store1.employees.create(first_name: "Hanna", last_name: "Oh", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Sara", last_name: "Kate", hourly_rate: 50)
+@store2.employees.create(first_name: "Nat", last_name: "Yuz", hourly_rate: 50)
+@store2.employees.create(first_name: "Caroline", last_name: "Jepps", hourly_rate: 65)
